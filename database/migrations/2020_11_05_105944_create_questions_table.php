f308697d162a67dac127e997030b4e2692e13de5
Chukwuemeka Victor Mbachu <victor.c.mbachu@gmail.com>
2020-11-05 12:23:14 +0100

    created question model and modified user model

---
+++ database/migrations/2020_11_05_105944_create_questions_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', static function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('body');
+            $table->unsignedBigInteger('views')->default(0);
+            $table->unsignedBigInteger('answers')->default(0);
+            $table->integer('votes')->default(0);
+            $table->unsignedBigInteger('best_answer_id')->nullable();
+
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->timestamps();
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
