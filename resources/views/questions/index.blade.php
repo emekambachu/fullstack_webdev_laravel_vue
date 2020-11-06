1c25974b4ab66a5ab8193857253cc1df11d0825c
Chukwuemeka Victor Mbachu <victor.c.mbachu@gmail.com>
2020-11-06 08:43:17 +0100

    worked on question page view

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -10,10 +10,34 @@
                     <div class="card-body">
                         @foreach($questions as $question)
                         <div class="media">
-                            <div class="media-body">
-                                <h3 class="mt-0">{{ $question->title }}</h3>
-                                {{ Illuminate\Support\str::limit($question->body, 250) }}
+                            <div class="d-flex flex-column counters">
+                                <div class="vote">
+                                    <strong>{{ $question->votes }}</strong>
+                                    {{ Illuminate\Support\str::plural('vote'), $question->votes }}
+                                </div>
+
+                                <div class="status {{ $question->status }}">
+                                    <strong>{{ $question->answers }}</strong>
+                                    {{ Illuminate\Support\str::plural('answer'), $question->answers }}
+                                </div>
+
+                                <div class="view">
+                                    {{ $question->views ." ". Illuminate\Support\str::plural('view'), $question->views }}
+                                </div>
                             </div>
+
+                                <div class="media-body">
+                                    <h3 class="mt-0">
+                                        <a href="{{ $question->url }}">
+                                            {{ $question->title }}</a></h3>
+                                    <p class="lead">
+                                        {{ Illuminate\Support\str::limit($question->body, 250) }}
+                                        Asked By<a href="{{ $question->user->url }}">
+                                            {{ $question->user->name }}</a>
+                                        <small class="text-muted">
+                                            {{ $question->created_at->diffForHumans() }}</small>
+                                    </p>
+                                </div>
                         </div>
                         <hr>
                         @endforeach
